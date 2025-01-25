04ebd2b6bc419ddd1ca9732a16781bfb29fb1497
Govinda835 <chaudharygovinda835@gmail.com>
2025-01-25 09:36:37 +0545

    delete single data

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ app.get("/home",(req,res)=>{
 
 
 app.post("/blog", upload.single("image"), async (req,res)=>{
-    console.log(req.body)
-    console.log(req.file)
+    //console.log(req.body)
+    //console.log(req.file)
     const {Name, Class, rollNo} = req.body
     const filename = req.file.filename
       
@@ -51,7 +51,31 @@ app.get("/blog",async (req,res)=>{
 })
 app.use(express.static("./storage"))
 
+app.get("/blog/:id",async (req,res)=>{
+    const id = req.params.id
+    const blog = await Blog.findById(id)
+    res.status(200).json({
+        message : "single data fetched successfully...",
+        data : blog
+    })
+})
+
+app.delete("/blog/:id",async (req,res)=>{
+    const id = req.params.id
+    const blog = await Blog.findByIdAndDelete(id)
+    if(!blog)
+    {
+        return res.status(400).json({
+            message : "please enter the correct id..."
+        })
+    }
+    res.status(200).json({
+        message : "single student record deleted successfully...",  
+    })
+    
+})
+
 
 app.listen(process.env.PORT, ()=>{
-    console.log("The server is running in port number 5000...")
+    console.log("The server is running in port number 5000....")
 })
